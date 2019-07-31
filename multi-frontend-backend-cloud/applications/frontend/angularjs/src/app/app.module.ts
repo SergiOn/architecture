a160dfcd5510f73e0d0d6f3dad73e066222d6960
SergiOn <onishhenko@gmail.com>
2019-07-31 09:33:34 +0300

    #multi-frontend-backend-cloud # frontend angularjs # updates

--- multi-frontend-backend-cloud/applications/frontend/angularjs/src/app/app.module.ts
+++ multi-frontend-backend-cloud/applications/frontend/angularjs/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import angular from 'angular';
 import AngularAnimate from 'angular-animate';
 import AngularMaterial  from 'angular-material';
+import 'angular-material/angular-material.css';
 import { ComponentsModule } from './pages/components.module';
 import { AppComponent } from './app.component';
 import { AppConfigModule } from './app.config.module';
-import 'angular-material/angular-material.css';
+import { GreetingModule } from './greeting/greeting.module';
 
 export const AppModule = angular.module('app' , [
     AppConfigModule,
     AngularMaterial,
     AngularAnimate,
-    ComponentsModule
+    ComponentsModule,
+    GreetingModule
 ])
 .component('app', AppComponent)
 .name;
